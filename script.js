d2db82f8cfcec5db0183e47a5c15ae1af11250d7
puyuyu1234 <145762224+puyuyu1234@users.noreply.github.com>
2024-10-02 00:45:08 +0900

    smartphoneButton

--- script.js
+++ script.js
@@ -386,3 +386,41 @@ let currentSceneID = 0;
 let currentScene = new GameScene(currentSceneID);
 let inputManager = new InputManager(canvas);
 requestAnimationFrame(loop);
+
+smartPhoneButton.addEventListener("click", () => {
+    const buttons = document.getElementsByClassName("spb");
+    for (const b of buttons) {
+        if (b.style.visibility == "visible") {
+            b.style.visibility = "collapse";
+        } else {
+            b.style.visibility = "visible";
+        }
+    }
+});
+
+const activeButtons = new Map();
+const keys = ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown", "r", " "];
+document.querySelectorAll(".spb").forEach((b, k) => {
+    b.addEventListener("contextmenu", (event) => {
+        event.preventDefault();
+    });
+    b.addEventListener("touchstart", (e) => {
+        const touch = e.changedTouches[0];
+        activeButtons.set(touch.identifier, [b, k]);
+        screenCanvas.dispatchEvent(new KeyboardEvent("keydown", { key: keys[k] }));
+        event.preventDefault();
+    });
+});
+
+document.addEventListener("touchend", (event) => {
+    for (let i = 0; i < event.changedTouches.length; i++) {
+        const touch = event.changedTouches[i];
+        const bk = activeButtons.get(touch.identifier);
+        if (bk == undefined) continue;
+        const [button, k] = bk;
+        if (button) {
+            screenCanvas.dispatchEvent(new KeyboardEvent("keyup", { key: keys[k] }));
+            activeButtons.delete(touch.identifier);
+        }
+    }
+});
